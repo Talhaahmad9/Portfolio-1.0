57f2558f5fd66a773a9455293b38e52f768b0932
Talhaahmad9 <talhaahmaed999@gmail.com>
2025-08-15 18:27:40 +0500

    Added profile photo and updated Home.jsx

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,7 +1,9 @@
+import profilePic from "../assets/profile-photo.jpg";
+
 export default function Home() {
   return (
     <section className="home container">
-      <img src="../assets/profile-photo.JPG" alt="profile-photo" />
+      <img src={profilePic} alt="Talha Ahmad" />
       <h3 id="name">Talha Ahmad</h3>
       <p className="intro">Hi, I’m a web developer with a passion for clean design and practical problem-solving. I build responsive websites using WordPress and React, and I’m currently exploring machine learning to expand my skillset. This portfolio is a glimpse into my work, my growth, and the creative energy I bring to every project.</p>
       <div class="cta-buttons">
